c06e817ecfacdc1f689d02fc35ce68415f8b8745
Deba-07 <dynamicdeba07@gmail.com>
2026-01-03 10:08:43 +0530

    add RealEstate contract with minting and total supply functionality

---
+++ contracts/RealEstate.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+contract RealEstate is ERC721URIStorage {
+    uint256 private _tokenIds;
+
+    constructor() ERC721("Real Estate", "REAL") {}
+
+    function mint(string memory tokenURI) public returns (uint256) {
+        _tokenIds++;
+        uint256 newItemId = _tokenIds;
+
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIds;
+    }
+}
