5383d8bebb14f85ce95a7ff745a95cf6a9c5ce0b
Deba-07 <dynamicdeba07@gmail.com>
2026-01-12 11:48:24 +0530

    feat: connect to MetaMask and load user account on app mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BrowserProvider } from 'ethers'
 
 const App = () => {
+  const [ account, setAccount ] = useState(null)
+
+  const loadBlockchainData = async () => {
+    const provider = new BrowserProvider(window.ethereum)
+    console.log(provider)
+    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+    setAccount(accounts[0])
+    console.log(accounts[0])
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
   return (
     <div className='text-3xl text-black font-bold text-center'>App</div>
   )
